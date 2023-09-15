042b3f8e0af376b08eeeec7f534449f5c3ccf07a
ferretlinux <144573051+ferretlinux@users.noreply.github.com>
2023-09-15 14:22:10 -0700

    Add files via upload

---
+++ collatz.js
@@ -0,0 +1,21 @@
+let input = process.argv[2]
+console.log(input)
+let seed = Number(input)
+if(input == "--help") {
+	console.log("just type node.js collatz.js (intager of your choosing), see its eazy!!!")
+} else if(input == "--credits") {
+	console.log("coded by ferretlinux23")
+	console.log("coded in JavaScript")
+} else if (input == "--version") {
+	console.log("1.0")
+} else {
+	while (seed != 1) {
+		if(seed % 2 == 1) {
+			seed = seed * 3 + 1
+			console.log(seed)
+		} else {
+			seed = seed / 2
+			console.log(seed)
+		}
+	}
+}
